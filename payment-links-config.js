24e316b70df051300659286cb8e0f4cc91283ab8
mat-prog310 <matthias.duprey1@gmail.com>
2026-09-21 11:21:29 +0200

    Delete payment-links-config.js

--- payment-links-config.js
+++
@@ -1,9 +0,0 @@
-
-// =============================================================================
-// payment-links-config.js - À inclure dans ton HTML AVANT stripe-service.js
-// =============================================================================
-
-const PAYMENT_LINKS = {};
-
-// Rendre disponible globalement
-window.PAYMENT_LINKS = PAYMENT_LINKS;
